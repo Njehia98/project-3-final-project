695ba29b5cb8915adff7f8b094dc17369333e5de
Njehia98 <stacynjihia028@gmail.com>
2024-05-08 20:07:26 +0300

    added more parameters to person.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from person import Person
 from address import Address
 
-# Your main functionality goes here
+# Main functionality goes here
 address1 = Address.create("Ngara", "Nairobi", "23568-00625")
 address2 = Address.create("Kikuyu", "Kiambu", "54987-230147")
-
+address3 = Address.create("Uthiru", "Kiambu", "54987-231147")
+address4 = Address.create("Kinoo", "Kiambu", "54987-230247")
 # Creating instances of Person
-person1 = Person.create("Stacy", 30, address1)
-person2 = Person.create("Ivan", 25, address2)
+person1 = Person.create("Stacy", 30, "0735642987","Mpesa", address1)
+person2 = Person.create("Ivan", 25, "0798423697","Card", address2)
+person3 = Person.create("Cynthia", 35, "0742367897","Cash on delivery", address3)
+person4 = Person.create("Israel", 18, "0710273274","Card", address4)
+
 
 # Deleting a person
 #Person.delete(person1)
@@ -16,6 +20,12 @@ person2 = Person.create("Ivan", 25, address2)
 all_addresses = Address.get_all()
 for address in all_addresses:
     print(address)
+
+    # Getting all names
+all_people = Person.get_all()
+for person in all_people:
+    print(person)
+
 
 # Finding a person by ID
 found_person = Person.find_by_id(id(person1))
@@ -29,6 +39,6 @@ copied_person = person2.copy()
 print(copied_person)
 
 # Searching for persons by name
-search_results = Person.search("Ivan")
+search_results = Person.search("Israel")
 for result in search_results:
     print(result)
--- person.py
+++ person.py
@@ -1,14 +1,16 @@
 class Person:
     people = []
 
-    def __init__(self, name, age, address=None):
+    def __init__(self, name, age, phone_number, payment_mode, address=None):
         self.name = name
         self.age = age
+        self.phone_number = phone_number
+        self.payment_mode = payment_mode
         self.address = address
 
     @classmethod
-    def create(cls, name, age, address=None):
-        person = cls(name, age, address)
+    def create(cls, name, age, phone_number, payment_mode, address=None):
+        person = cls(name, age, phone_number, payment_mode, address)
         cls.people.append(person)
         return person
 
@@ -28,11 +30,11 @@ class Person:
         return None
 
     def copy(self):
-        return Person(self.name, self.age, self.address.copy() if self.address else None)
+        return Person(self.name, self.age, self.phone_number, self.payment_mode, self.address.copy() if self.address else None)
 
     @classmethod
     def search(cls, keyword):
         return [person for person in cls.people if keyword in person.name]
 
     def __str__(self):
-        return f"Name: {self.name}, Age: {self.age}, Address: {self.address}"
+        return f"Name: {self.name}, Age: {self.age}, Phone Number: {self.phone_number}, Payment Mode: {self.payment_mode}, Address: {self.address}"
